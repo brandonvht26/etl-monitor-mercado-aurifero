d2ae2d36b15946da29b645281a5ea21dbd4a9adb
brandonvht26 <brandonvhuerat@gmail.com>
2026-09-12 23:11:26 -0500

    Módulo de Bases de Datos y correciones menores

--- database.py
+++ database.py
@@ -1,19 +1,39 @@
 import sqlite3 as sql
 
-def createDB():
-    conn = sql.connect("mercado_aurifero_db")
+def createTable():
+    conn = sql.connect("mercado_aurifero.db")
+    cur = conn.cursor()
+    
+    script = """
+    CREATE TABLE IF NOT EXISTS gold_price_history (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        datetime TEXT NOT NULL,
+        price REAL NOT NULL
+    );
+    """
+
+    cur.execute(
+        script
+    )
+
     conn.commit()
     conn.close()
 
-def createTable():
-    conn = sql.connect("mercado_aurifero_db")
-    cursor = conn.cursor()
-    
-    cursor.execute(
-        """CREATE TABLE gold_price_history IF NOT EXISTS(
-            id int PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
-            datetime TEXT NOT NULL,
-            price REAL NOT NULL
-        )
-        """
+def insert_gold_price(datetime, price):
+    data = (datetime, price)
+
+    conn = sql.connect("mercado_aurifero.db")
+    cur = conn.cursor()
+
+    script = "INSERT INTO gold_price_history (datetime, price) VALUES (?, ?)"
+
+    cur.execute(
+        script,
+        data
     )
+    
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    createTable()
